17d3d81996b7ca2b34120f7061022c0245fc19ac
itsmohsinzaidi313 <mozak099@gmail.com>
2025-11-10 22:01:29 +0500

    fixes

--- CreateOrderService/RequestQueueListener.cs
+++ CreateOrderService/RequestQueueListener.cs
@@ -3,7 +3,7 @@ using PointofSaleModels.Services;
 
 namespace CreateOrderService
 {
-    internal class RequestQueueListener(ILogger<RequestQueueListener> logger, RabbitMqConnection rabbitConnection, RequestQueueAction listener) : RabbitMqConsumerService<RequestQueueListener>(logger, rabbitConnection, listener)
+    internal class RequestQueueListener(ILogger<RequestQueueListener> logger, RabbitMqConnection rabbitConnection, IQueueAction listener) : RabbitMqConsumerService<RequestQueueListener>(logger, rabbitConnection, listener)
     {
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
