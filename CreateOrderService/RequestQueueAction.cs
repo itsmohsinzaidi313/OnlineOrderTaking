db942507bee5940550748d38e32cdaadd3a4d8bd
itsmohsinzaidi313 <mozak099@gmail.com>
2026-01-31 22:15:51 +0500

    fixes

--- CreateOrderService/RequestQueueAction.cs
+++ CreateOrderService/RequestQueueAction.cs
@@ -25,7 +25,6 @@ namespace CreateOrderService
 
         private async Task<string> GetConnectionString(string domainName)
         {
-            return "Host=85.190.242.39;Port=5433;Database=rollinnbbq;Username=postgres;Password=postgrespass";
             var restaurant = await context.Restaurants.FirstOrDefaultAsync(r => r.DomainName == domainName);
             return restaurant?.ConnectionString ?? throw new Exception("Restaurant not found");
         }
