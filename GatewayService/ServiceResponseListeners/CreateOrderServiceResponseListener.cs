083df5358929e6cca0b6c726880b3897fab3fa55
itsmohsinzaidi313 <mozak099@gmail.com>
2026-02-17 19:55:55 +0500

    working

--- GatewayService/ServiceResponseListeners/CreateOrderServiceResponseListener.cs
+++ GatewayService/ServiceResponseListeners/CreateOrderServiceResponseListener.cs
@@ -1,15 +1,23 @@
 ﻿using PointofSaleModels.ServicePayloads;
 using PointofSaleModels.Services;
 using PointofSaleModels.Settings;
+using StackExchange.Redis;
+using System.Text.Json;
 
 namespace GatewayService.ServiceResponseListeners
 {
-    public class CreateOrderServiceResponseListener(ILogger<CreateOrderServiceResponseListener> logger, RabbitMqConnection rabbitConnection, Implementation implementation) : RabbitMqConsumerService<CreateOrderServiceResponseListener>(logger, rabbitConnection)
+    public class CreateOrderServiceResponseListener(ILogger<CreateOrderServiceResponseListener> logger, RabbitMqConnection rabbitConnection, Implementation implementation, IConnectionMultiplexer redis) : RabbitMqConsumerService<CreateOrderServiceResponseListener>(logger, rabbitConnection)
     {
         public override string QueueName() => RabbitMqQueues.OrderResponseQueue;
         public override async Task OnMessage(string svcPayload)
         {
             await implementation.SendToUser<OrderServicePayload>(svcPayload);
+            using var doc = JsonDocument.Parse(svcPayload);
+            var root = doc.RootElement;
+            var clientId = root.GetProperty("UserId").GetString() ?? throw new Exception("UserId not found");
+            var orderNumber = root.GetProperty("DataPayload").GetProperty("OrderNumber").GetString() ?? throw new Exception("OrderNumber not found");
+            var db = redis.GetDatabase();
+            await db.StringSetAsync($"order:{orderNumber}:{clientId}", svcPayload);
         }
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
--- GatewayService/ServiceResponseListeners/OrderStatusServiceResponseListener.cs
+++ GatewayService/ServiceResponseListeners/OrderStatusServiceResponseListener.cs
@@ -1,15 +1,32 @@
 ﻿using PointofSaleModels.ServicePayloads;
 using PointofSaleModels.Services;
 using PointofSaleModels.Settings;
+using StackExchange.Redis;
+using System.Text.Json;
 
 namespace GatewayService.ServiceResponseListeners
 {
-    public class OrderStatusServiceResponseListener(ILogger<OrderStatusServiceResponseListener> logger, RabbitMqConnection rabbitConnection, Implementation implementation) : RabbitMqConsumerService<OrderStatusServiceResponseListener>(logger, rabbitConnection)
+    public class OrderStatusServiceResponseListener(ILogger<OrderStatusServiceResponseListener> logger, RabbitMqConnection rabbitConnection, Implementation implementation, IConnectionMultiplexer redis) : RabbitMqConsumerService<OrderStatusServiceResponseListener>(logger, rabbitConnection)
     {
         public override string QueueName() => RabbitMqQueues.OrderStatusResponseQueue;
         public override async Task OnMessage(string svcPayload)
         {
             await implementation.SendToUser<OrderStatusPayload>(svcPayload);
+            using var doc = JsonDocument.Parse(svcPayload);
+            var root = doc.RootElement;
+            var orderNumber = root.GetProperty("OrderNumber").GetString() ?? throw new Exception("OrderNumber not found");
+            var db = redis.GetDatabase();
+            var server = redis.GetServer(redis.GetEndPoints().First());
+            var payload = JsonSerializer.Deserialize<OrderStatusPayload>(svcPayload);
+            foreach (var key in server.Keys(pattern: $"order:{orderNumber}:*"))
+            {
+                var clientId = key.ToString().Split(':')[2];
+                if (string.IsNullOrEmpty(clientId))
+                {
+                    continue;
+                }
+                await implementation.SendToUser<OrderStatusPayload>("OrderStatusUpdate", clientId, payload);
+            }
         }
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
