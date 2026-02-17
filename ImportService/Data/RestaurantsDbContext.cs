49ccf3cbb982b72f81d0299ae28aaafdc44a3a15
itsmohsinzaidi313 <mozak099@gmail.com>
2026-02-17 18:54:30 +0500

    splitted dbcontexts

---
+++ ImportService/Data/RestaurantsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ImportService.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImportService.Data
+{
+    public class RestaurantsDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Restaurants> Restaurants => Set<Restaurants>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Restaurants>()
+                .ToTable("restaurants")
+                .HasKey(x => x.Id);
+        }
+    }
+}
