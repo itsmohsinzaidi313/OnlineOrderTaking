6a876bbc14c22e327c7bfeb337fd3bb4a3ac9bc4
itsmohsinzaidi313 <mozak099@gmail.com>
2026-02-20 22:58:10 +0500

    changed port 5000 to 5005

--- GatewayService/Program.cs
+++ GatewayService/Program.cs
@@ -90,7 +90,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     });
 
 builder.Services.AddAuthorization();
-builder.Services.AddGrpcClient<PushNotificationServiceClient>(o => o.Address = new Uri("http://pushnotificationservice:5000"));
+builder.Services.AddGrpcClient<PushNotificationServiceClient>(o => o.Address = new Uri("http://pushnotificationservice:5005"));
 
 var app = builder.Build();
 app.UseRouting();
