0bea1a2cd14c9b87c15acec394a91ff0e1f037f4
itsmohsinzaidi313 <mozak099@gmail.com>
2026-02-22 03:43:32 +0500

    added logging

--- PushNotificationService/PushNotificationServiceImpl.cs
+++ PushNotificationService/PushNotificationServiceImpl.cs
@@ -8,19 +8,29 @@ using static PointofSaleModels.Protos.PushNotificationService;
 
 namespace PushNotificationService
 {
-    public class PushNotificationServiceImpl(WebPushService service, IConnectionMultiplexer multiplexer) : PushNotificationServiceBase
+    public class PushNotificationServiceImpl(ILogger<PushNotificationServiceImpl> logger, WebPushService service, IConnectionMultiplexer multiplexer) : PushNotificationServiceBase
     {
         private readonly IDatabase _db = multiplexer.GetDatabase();
         public override async Task<PushNotificationNotifyResponse> Notify(PushNotificationNotifyRequest request, ServerCallContext context)
         {
-            var subscriptionJson = _db.StringGet($"subscription:{request.ClientId}");
-            var subscription = JsonSerializer.Deserialize<PushSubscriptionDto>(subscriptionJson.ToString());
-            if (subscription == null)
+            var subscriptionJson = _db.StringGet($"subscription:{request.ClientId}").ToString();
+            if(string.IsNullOrEmpty(subscriptionJson))
             {
+                logger.LogInformation("Subscription not found for client {ClientId}", request.ClientId);
                 return new PushNotificationNotifyResponse
                 {
                     Success = false,
                     Message = "Subscription not found for the client"
+                };
+            }
+            var subscription = JsonSerializer.Deserialize<PushSubscriptionDto>(subscriptionJson);
+            if (subscription == null)
+            {
+                logger.LogError("Failed to deserialize subscription for client {ClientId}", request.ClientId);
+                return new PushNotificationNotifyResponse
+                {
+                    Success = false,
+                    Message = "Subscription corrupted for the client"
                 };
             }
             var pushSubscribtion = new PushSubscription
@@ -39,6 +49,7 @@ namespace PushNotificationService
             });
             var pushMessage = new PushMessage(content);
             await service.SendAsync(pushSubscribtion, pushMessage);
+            logger.LogInformation("Notification sent successfully to client {ClientId}", request.ClientId);
             var response = new PushNotificationNotifyResponse
             {
                 Success = true,
@@ -59,6 +70,7 @@ namespace PushNotificationService
             };
             var json = System.Text.Json.JsonSerializer.Serialize(pushSubscribtion);
             await _db.StringSetAsync($"subscription:{clientId}", json);
+            logger.LogInformation("Client {ClientId} subscribed successfully", clientId);
             var response = new PushNotificationSubscriptionResponse
             {
                 Success = true,
@@ -71,6 +83,7 @@ namespace PushNotificationService
         {
             var clientId = request.ClientId;
             await _db.KeyDeleteAsync($"subscription:{clientId}");
+            logger.LogInformation("Client {ClientId} unsubscribed successfully", clientId);
             var response = new PushNotificationUnsubscribeResponse
             {
                 Success = true,
