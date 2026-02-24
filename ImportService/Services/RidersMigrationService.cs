f3d40674c89daed8596e1d0151fcc7502bc3a088
itsmohsinzaidi313 <mozak099@gmail.com>
2026-02-24 07:25:06 +0500

    fix

--- ImportService/Services/RidersMigrationService.cs
+++ ImportService/Services/RidersMigrationService.cs
@@ -9,6 +9,7 @@ namespace ImportService.Services
         public async Task MigrateAsync(PostgresDbContext pgDb, int companyId = 0, CancellationToken ct = default)
         {
             var riders = await sqlDb.Riders
+                .Where(x => x.IsActive == true && x.BranchId != null && x.RiderName != null)
                 .Join(sqlDb.BranchMasters, a => a.BranchId, b => b.BranchId, (a, b) => new { Rider = a, Branch = b })
                 .Where(x => x.Branch.CompanyId == companyId)
                 .Select(x => x.Rider)
