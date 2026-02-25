51259ea23f797732335c3f17c64f2bb78433850a
itsmohsinzaidi313 <mozak099@gmail.com>
2026-02-25 17:23:03 +0500

    renamed orderstatuspayload to orderupdatepayload

--- PointofSaleModels/ServicePayloads/OrderUpdatePayload.cs
+++ PointofSaleModels/ServicePayloads/OrderUpdatePayload.cs
@@ -1,21 +1,24 @@
 ﻿namespace PointofSaleModels.ServicePayloads
 {
-    public class OrderStatusPayload : ServicePayload
+    public class OrderUpdatePayload : ServicePayload
     {
-        public OrderStatusPayload() : base()
+        public OrderUpdatePayload() : base()
         {
         }
 
-        public OrderStatusPayload(OrderStatusPayload payload) : base(payload)
+        public OrderUpdatePayload(OrderUpdatePayload payload) : base(payload)
         {
             OrderNumber = payload.OrderNumber;
             OrderStatusId = payload.OrderStatusId;
             BranchTransferId = payload.BranchTransferId;
+            RiderId = payload.RiderId;
+            DeliveryTime = payload.DeliveryTime;
         }
         public string OrderNumber { get; set; }
         public int? OrderStatusId { get; set; }
         public int? BranchTransferId { get; set; }
         public int? RiderId { get; set; }
+        public int? DeliveryTime { get; set; }
         public object? DataPayload { get; set; }
     }
 }
