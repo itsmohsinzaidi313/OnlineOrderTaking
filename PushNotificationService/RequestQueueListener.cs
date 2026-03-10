4c45c3fa4fb5e4a94a41d9155addfc156e301d09
itsmohsinzaidi313 <mozak099@gmail.com>
2026-03-10 22:42:39 +0500

    fix: push notification service

--- PushNotificationService/RequestQueueListener.cs
+++ PushNotificationService/RequestQueueListener.cs
@@ -17,52 +17,63 @@ namespace PushNotificationService
         public override async Task OnMessage(string payload)
         {
             var request = JsonSerializer.Deserialize<PushNotificationServicePayload>(payload);
-            try
-            {
-                var endpoint = multiplexer.GetEndPoints().First();
-                var server = multiplexer.GetServer(endpoint);
-                var keys = server.Keys(pattern: $"subscription:{request.ClientId}");
-                logger.LogInformation("Processing push notification request for pattern {ClientId} Total {Count}", request?.ClientId, keys.Count());
+            var endpoint = multiplexer.GetEndPoints().First();
+            var server = multiplexer.GetServer(endpoint);
+            var keys = server.Keys(pattern: $"subscription:{request.ClientId}");
+            logger.LogInformation("Processing push notification request for pattern {ClientId} Total {Count}", request?.ClientId, keys.Count());
 
-                foreach (var clientId in keys)
+            foreach (var clientId in keys)
+            {
+                var cid = clientId.ToString();
+                var redisValue = await _db.StringGetAsync(cid);
+                if (!redisValue.HasValue)
                 {
-                    var cid = clientId.ToString();
-                    var redisValue = await _db.StringGetAsync(cid);
-                    if (!redisValue.HasValue)
-                    {
-                        logger.LogWarning("No subscription found for client {ClientId}", cid);
-                        return;
-                    }
-                    var subscription = JsonSerializer.Deserialize<PushSubscriptionDto>(redisValue.ToString());
-                    if (subscription == null)
-                    {
-                        logger.LogError("Subscription data is corrupted for client {ClientId}", cid);
-                        return;
-                    }
-                    var pushSubscribtion = new PushSubscription
-                    {
-                        Endpoint = subscription.Endpoint,
-                        Keys = new Dictionary<string, string>
+                    logger.LogWarning("No subscription found for client {ClientId}", cid);
+                    return;
+                }
+                var subscription = JsonSerializer.Deserialize<PushSubscriptionDto>(redisValue.ToString());
+                if (subscription == null)
+                {
+                    logger.LogError("Subscription data is corrupted for client {ClientId}", cid);
+                    return;
+                }
+                var pushSubscription = new PushSubscription
+                {
+                    Endpoint = subscription.Endpoint,
+                    Keys = new Dictionary<string, string>
                         {
                             { "p256dh", subscription.P256DH },
                             { "auth", subscription.Auth }
                         }
-                    };
-                    var content = JsonSerializer.Serialize(new
-                    {
-                        title = request.Title,
-                        message = request.Message
-                    });
-                    var pushMessage = new PushMessage(content);
-                    await pushService.SendAsync(pushSubscribtion, pushMessage);
+                };
+                var content = JsonSerializer.Serialize(new
+                {
+                    title = request.Title,
+                    message = request.Message
+                });
+                var pushMessage = new PushMessage(content);
+                try
+                {
+                    await pushService.SendAsync(pushSubscription, pushMessage);
                     logger.LogInformation("Successfully processed push notification request for client {ClientId}", cid);
 
                 }
+                catch (PushServiceClientException clientException)
+                {
+                    logger.LogError(clientException, "Error sending push notification to client {ClientId}", cid);
+                    if (clientException.StatusCode == System.Net.HttpStatusCode.Gone || clientException.StatusCode == System.Net.HttpStatusCode.NotFound)
+                    {
+                        logger.LogInformation("Removing subscription for client {ClientId} due to invalid endpoint", cid);
+                        await _db.KeyDeleteAsync(cid);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "Error processing push notification request for client {ClientId}", request.ClientId);
+                }
             }
-            catch (Exception ex)
-            {
-                logger.LogError(ex, "Error processing push notification request for client {ClientId}", request.ClientId);
-            }
+
+
         }
 
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
