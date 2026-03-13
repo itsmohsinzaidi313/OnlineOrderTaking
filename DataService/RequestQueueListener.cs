c263bc08ccb2f6130f086777960397a7e7d40676
itsmohsinzaidi313 <mozak099@gmail.com>
2026-03-13 18:47:07 +0500

    fix

--- DataService/RequestQueueListener.cs
+++ DataService/RequestQueueListener.cs
@@ -39,7 +39,7 @@ namespace DataService
                 {
                     if (!requestPayload.OrderUserId.HasValue) throw new Exception("UserId missing for userwise orders list");
 
-                    var orders = impl.GetOrdersAsync(connectionString, requestPayload.OrderUserId.Value).ToListAsync();
+                    var orders = await impl.GetOrdersAsync(connectionString, requestPayload.OrderUserId.Value).ToListAsync();
                     var orderStatuses = await impl.GetOrderStatusesAsync(connectionString);
                     var riders = await impl.GetRidersAsync(requestPayload.OrderUserId.Value, connectionString);
                     var branches = await impl.GetBranchesAsync(connectionString);
