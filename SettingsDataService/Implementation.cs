165fa46a102bedfcea6cb517d26c727d3951a1d5
Mohsin Zaidi <mozak099@gmail.com>
2026-04-01 17:48:22 +0500

    changes

--- SettingsDataService/Implementation.cs
+++ SettingsDataService/Implementation.cs
@@ -93,38 +93,9 @@ internal class Implementation()
                         ["EndTime"] = dayMapping.EndTime.ToString(),
                     };
                     bussinessDays.Add(businessday);
-                    var isBranchOpen = false;
-                    var value = days[dayMapping.DayId]?.ToString() ?? string.Empty;
-                    if (value == todayDow)
-                    {
-                        var startTime = dayMapping.StartTime;
-                        var endTime = dayMapping.EndTime;
-                        var timeOfDay = DateTime.Now.TimeOfDay;
-                        if (startTime > endTime)
-                        {
-                            var maybeOpen = startTime > timeOfDay;
-                            if (maybeOpen)
-                            {
-                                isBranchOpen = true;
-                            }
-                            else
-                            {
-                                isBranchOpen = timeOfDay > endTime;
-                            }
-                        }
-                        else if (startTime < endTime)
-                        {
-                            if (timeOfDay >= startTime && timeOfDay <= endTime)
-                            {
-                                isBranchOpen = true;
-                            }
-                        }
-                        item2["IsBranchOpen"] = isBranchOpen;
-                        break;
-                    }
                 }
                 item2["BusinessDays"] = bussinessDays;
-                item2["IsBranchOpen"] = false;
+                item2["IsBranchOpen"] = CalculateBranchOpenStatus(bussinessDays);
 
                 var branchDetail = branchDetails.FirstOrDefault(bd => bd.BranchId == branchId);
                 if (branchDetail != null)
@@ -165,6 +136,63 @@ internal class Implementation()
         };
         orderModes["Theme"] = themeData;
         return orderModes;
+    }
+
+    private static bool CalculateBranchOpenStatus(JsonArray? businessTimes)
+    {
+        if (businessTimes is null || businessTimes.Count == 0)
+        {
+            return false;
+        }
+
+        var now = DateTime.Now;
+        var currentDay = now.DayOfWeek;
+        var businessTimeNode = businessTimes.FirstOrDefault(x => x["Day"]?.GetValue<string>()?.Equals(currentDay.ToString(), StringComparison.OrdinalIgnoreCase) == true);
+
+        if (businessTimeNode is not JsonObject businessTime)
+            return false;
+
+        var day = businessTime["Day"]?.GetValue<string>();
+        if (!Enum.TryParse<DayOfWeek>(day, true, out var businessDay))
+            return false;
+
+        if (!TryParseTime(businessTime["StartTime"], out var startTime) || !TryParseTime(businessTime["EndTime"], out var endTime))
+            return false;
+
+        var isBranchOpen = false;
+        var timeOfDay = DateTime.Now.TimeOfDay;
+        if (startTime > endTime)
+        {
+            var maybeOpen = startTime > timeOfDay;
+            if (maybeOpen)
+            {
+                isBranchOpen = true;
+            }
+            else
+            {
+                isBranchOpen = timeOfDay > endTime;
+            }
+        }
+        else if (startTime < endTime)
+        {
+            if (timeOfDay >= startTime && timeOfDay <= endTime)
+            {
+                isBranchOpen = true;
+            }
+        }
+        return isBranchOpen;
+    }
+
+    private static bool TryParseTime(JsonNode? timeNode, out TimeSpan time)
+    {
+        time = default;
+        if (timeNode is null)
+        {
+            return false;
+        }
+
+        var timeValue = timeNode.GetValue<string>();
+        return TimeSpan.TryParse(timeValue, out time);
     }
 
     private static async Task<JsonObject> GetThemeDataAsync(string connectionString)
