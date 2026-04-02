b52b0928d0aa0750f466d6535250d765fb2d06eb
Mohsin Zaidi <mozak099@gmail.com>
2026-04-03 04:33:37 +0500

    fixes

--- ExportService/DatabaseContexts/PostgresDbContext.cs
+++ ExportService/DatabaseContexts/PostgresDbContext.cs
@@ -279,6 +279,7 @@ namespace ExportService.DatabaseContexts
                 .HasKey(x => x.RiderId);
 
             modelBuilder.Entity<OrderStatusLog>()
+                .Ignore(x => x.CreatedBy)
                 .Ignore(x => x.IsActive);
         }
     }
