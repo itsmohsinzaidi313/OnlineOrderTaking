4b4534b5af8b5def6a312463262bd44456ce2aa6
Mohsin Zaidi <mozak099@gmail.com>
2026-06-25 21:03:28 +0500

    changes

--- GatewayService/ServiceResponseListeners/CreateOrderServiceResponseListener.cs
+++ GatewayService/ServiceResponseListeners/CreateOrderServiceResponseListener.cs
@@ -11,11 +11,13 @@ namespace GatewayService.ServiceResponseListeners
         public override string QueueName() => RabbitMqQueues.OrderResponseQueue;
         public override async Task OnMessage(string svcPayload)
         {
-            await implementation.SendToUser<OrderServicePayload>(svcPayload);
             using var doc = JsonDocument.Parse(svcPayload);
             var root = doc.RootElement;
             var clientId = root.GetProperty("UserId").GetString() ?? throw new Exception("UserId not found");
             var orderNumber = root.GetProperty("DataPayload").GetProperty("OrderNumber").GetString() ?? throw new Exception("OrderNumber not found");
+            var signalrMethodName = root.GetProperty("SignalRMethodName").GetString() ?? throw new Exception("SignalRMethodName not found");
+            logger.LogInformation("Received order response for order {OrderNumber} and user {UserId}\n{svcPayload}", orderNumber, clientId, svcPayload);
+            await implementation.SendToUser<OrderServicePayload>(svcPayload);
             var db = redis.GetDatabase();
             await db.StringSetAsync($"order:{orderNumber}:{clientId}", svcPayload, expiry: TimeSpan.FromHours(2));
         }
