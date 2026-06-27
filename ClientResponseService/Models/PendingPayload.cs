86378d81723d0c3dd9963f10cc9e155f2a29ea22
Mohsin Zaidi <mozak099@gmail.com>
2026-06-27 14:56:57 +0500

    changes

--- ClientResponseService/Models/PendingPayload.cs
+++
@@ -1,9 +0,0 @@
-﻿using PointofSaleModels.ServicePayloads;
-
-namespace ClientResponseService.Models
-{
-    public class PendingPayload<T> where T : ServicePayload
-    {
-        public T Payload { get; set; } = null!;
-    }
-}
